4e77a4f79097571599cbba5e71e87c9d745daf3f
philipe Amorim <philipe.lopes07@gmail.com>
2018-07-24 18:44:40 -0300

    ajustes no endpoint /categoria/id

--- src/main/java/com/philipe/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/philipe/cursomc/resources/CategoriaResource.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 import com.philipe.cursomc.domain.Categoria;
 import com.philipe.cursomc.services.CategoriaService;
 
+
+
+
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
@@ -20,8 +23,8 @@ public class CategoriaResource {
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
 	public ResponseEntity<?> find(@PathVariable Integer id) {
 		
-		Categoria obj = service.buscar(id);
 		
+		Categoria obj = service.buscar(id);
 		return ResponseEntity.ok().body(obj);
 		
 	}
--- src/main/java/com/philipe/cursomc/services/CategoriaService.java
+++ src/main/java/com/philipe/cursomc/services/CategoriaService.java
@@ -7,6 +7,9 @@ import org.springframework.stereotype.Service;
 
 import com.philipe.cursomc.domain.Categoria;
 import com.philipe.cursomc.repository.CategoriaRepository;
+import com.philipe.cursomc.services.exceptions.ObjectNotFoundException;
+
+
 
 @Service
 public class CategoriaService {
@@ -16,6 +19,6 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return  obj.orElse(null);
+		return  obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! " +id + ", Tipo "+ Categoria.class.getName()));
 	}
 }
